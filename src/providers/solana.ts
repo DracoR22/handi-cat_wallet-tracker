6d5515ff943081341aa975485be26b4aeaeb3123
DracoR22 <rdraco039@gmail.com>
2024-11-10 10:00:37 -0500

    updated tutorial

--- src/providers/solana.ts
+++ src/providers/solana.ts
@@ -6,8 +6,8 @@ dotenv.config()
 const SOLANA_NETWORK = clusterApiUrl('mainnet-beta')
 const HELIUS_NETWORK = `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`
 const ALCHEMY_NETWORK = `https://solana-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
-const CHAINSTACK_NETWORK = `https://solana-mainnet.core.chainstack.com/${process.env.CHAINSTACK_API_KEY}`
-const CHAINSTACK_NETWORK2 = `https://solana-mainnet.core.chainstack.com/${process.env.CHAINSTACK_API_KEY2}`
+const CHAINSTACK_NETWORK = `https://solana-mainnet.core.chainstack.com/${process.env.CHAINSTACK_API_KEY2}`
+const CHAINSTACK_NETWORK2 = `https://solana-mainnet.core.chainstack.com/${process.env.CHAINSTACK_API_KEY}`
 
 export const connection = new Connection(CHAINSTACK_NETWORK, 'confirmed')
 // export const connection2 = new Connection(CHAINSTACK_NETWORK2, 'confirmed')
