b05ffd11e7f31b0d7fce299b9ceb1a067935c93f
DracoR22 <rdraco039@gmail.com>
2024-11-18 12:47:09 -0500

    changed api key

--- src/providers/solana.ts
+++ src/providers/solana.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 const SOLANA_NETWORK = clusterApiUrl('mainnet-beta')
 const HELIUS_NETWORK = `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`
-const CHAINSTACK_NETWORK = `https://solana-mainnet.core.chainstack.com/${process.env.CHAINSTACK_API_KEY2}`
+const CHAINSTACK_NETWORK = `https://solana-mainnet.core.chainstack.com/${process.env.CHAINSTACK_API_KEY}`
 const QUICKNODE_NETWORK = process.env.QUICKNODE_API_KEY ?? ''
 
 export const connection = new Connection(CHAINSTACK_NETWORK, 'confirmed')
