888482c375d4144a97ad8ac890e3506b6747ba7f
DracoR22 <rdraco039@gmail.com>
2024-11-27 12:21:02 -0500

    trying syndica network

--- src/providers/solana.ts
+++ src/providers/solana.ts
@@ -6,7 +6,8 @@ dotenv.config()
 const SOLANA_NETWORK = clusterApiUrl('mainnet-beta')
 const HELIUS_NETWORK = `https://mainnet.helius-rpc.com/?api-key=${process.env.HELIUS_API_KEY}`
 const CHAINSTACK_NETWORK = `https://solana-mainnet.core.chainstack.com/${process.env.CHAINSTACK_API_KEY}`
+const SYNDICA_NETWORK = `https://solana-mainnet.api.syndica.io/api-key/${process.env.SYNDICA_API_KEY}`
 const QUICKNODE_NETWORK = process.env.QUICKNODE_API_KEY ?? ''
 
-export const connection = new Connection(SOLANA_NETWORK, 'confirmed')
+export const connection = new Connection(SYNDICA_NETWORK, 'confirmed')
 // export const connection2 = new Connection(CHAINSTACK_NETWORK2, 'confirmed')
