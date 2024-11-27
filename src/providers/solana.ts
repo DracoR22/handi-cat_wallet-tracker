4102a60cd06fea1584756df024bf189ff66339be
DracoR22 <rdraco039@gmail.com>
2024-11-27 12:25:07 -0500

    nope

--- src/providers/solana.ts
+++ src/providers/solana.ts
@@ -9,5 +9,5 @@ const CHAINSTACK_NETWORK = `https://solana-mainnet.core.chainstack.com/${process
 const SYNDICA_NETWORK = `https://solana-mainnet.api.syndica.io/api-key/${process.env.SYNDICA_API_KEY}`
 const QUICKNODE_NETWORK = process.env.QUICKNODE_API_KEY ?? ''
 
-export const connection = new Connection(SYNDICA_NETWORK, 'confirmed')
+export const connection = new Connection(CHAINSTACK_NETWORK, 'confirmed')
 // export const connection2 = new Connection(CHAINSTACK_NETWORK2, 'confirmed')
