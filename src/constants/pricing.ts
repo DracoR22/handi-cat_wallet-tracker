5d0811c6985f81db2fd3fc69273cfa5cfbe34880
DracoR22 <rdraco039@gmail.com>
2025-01-02 21:41:25 -0500

    small number fix

--- src/constants/pricing.ts
+++ src/constants/pricing.ts
@@ -3,7 +3,7 @@ export const MAX_HOBBY_WALLETS = 50
 export const MAX_PRO_WALLETS = 100
 export const MAX_WHALE_WALLETS = 220
 
-export const HOBBY_PLAN_FEE = 0.2 * 1e9
+export const HOBBY_PLAN_FEE = 0.1 * 1e9
 export const PRO_PLAN_FEE = 0.4 * 1e9
 export const WHALE_PLAN_FEE = 0.8 * 1e9
 
